63443b1805687b5f10b15e16bf2291b141e030d3
Pete Corey <petecorey@gmail.com>
2016-06-30 09:13:32 -0400

    # Keep it Simple
    Interestingly, our incredibly simple and incredibly incorrect initial
    solution for `deleteNth` holds up under additional base case tests. Any
    calls to `deleteNth` with a zero value for `N` will result in an empty
    array.

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ describe("deleteNth", function() {
 
     it("deletes occurrences of an element if it occurs more than n times", function () {
         expect(deleteNth([], 0)).to.deep.equal([]);
+        expect(deleteNth([1, 2], 0)).to.deep.equal([]);
     });
 
 });
