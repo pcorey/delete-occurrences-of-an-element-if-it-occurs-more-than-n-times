775ef33bee719579eb68d9b9333d7f7e3f38aa60
Pete Corey <petecorey@gmail.com>
2016-06-30 09:51:17 -0400

    # Final Tests
    At this point, I think this solution solves the problem at hand for any
    given inputs. As a final test, I add the two test cases provided in the
    kata description.
    
    Both of these tests pass. Victory!

--- test/index.js
+++ test/index.js
@@ -9,6 +9,9 @@ describe("deleteNth", function() {
 
         expect(deleteNth([1, 2], 1)).to.deep.equal([1, 2]);
         expect(deleteNth([1, 1, 2], 1)).to.deep.equal([1, 2]);
+
+        expect(deleteNth([20, 37, 20, 21], 1)).to.deep.equal([20, 37, 21]);
+        expect(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3)).to.deep.equal([1, 1, 3, 3, 7, 2, 2, 2]);
     });
 
 });
