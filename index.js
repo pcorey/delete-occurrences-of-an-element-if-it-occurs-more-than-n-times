0a86271899af3b808f4116455aa37da8c40cd7b0
Pete Corey <petecorey@gmail.com>
2016-06-30 17:39:02 -0400

    # Final Refactoring
    Now that our tests are green and we're satisfied with the overall shape
    of our final solution, we can do some final refactoring.
    
    Using the underrated [tilde
    operator](http://stackoverflow.com/a/5971668), we can simplify our
    `seen` increment step and merge it onto the same line as the comparison
    against `x`. Next, we can leverage some ES6 syntax sugar to consolidate
    our `filter` lambda onto a single line.

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 export function deleteNth(arr,x){
     let seen = {};
-    return arr.filter((num) => {
-        seen[num] = (seen[num] || 0) + 1;
-        return seen[num] <= x;
-    });
+    return arr.filter((num) => (seen[num] = ~~seen[num] + 1) <= x);
 }
